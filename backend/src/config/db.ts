c71d96c1bc85a70cb94766091b3b4323e4ada3b7
sam <samkele@bbd.co.za>
2025-05-07 00:17:11 +0200

    update be: changed table name

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -11,7 +11,7 @@ const db = pgp({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  ssl: true
+  ssl: { rejectUnauthorized: false }
 });
 
 export default db;
