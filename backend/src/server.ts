e648e7c98679aa85565b0abc29be5686da1c76b9
sam <samkele@bbd.co.za>
2025-05-06 20:16:30 +0200

    update fe: added host

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,6 +10,6 @@ const PORT = process.env.PORT || 3000;
 console.log(`Environment: ${process.env.NODE_ENV}`);
 console.log(`Server running on port ${PORT}`);
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',() => {
   console.log(`🚀 Listening on port ${PORT}`);
 });
