7e8a4d8f30234c3d4947ed6eae2442d16a8a7e00
Franco du Buisson <franco.dubuisson@bbd.co.za>
2025-05-08 20:55:45 +0200

    fixed return type to not use any type

---
+++ backend/src/types/auth.d.ts
@@ -0,0 +1,12 @@
+export interface JwtPayload {
+    id: string;
+    email: string;
+    verified_email: boolean;
+    name: string;
+    given_name: string;
+    family_name: string;
+    picture: string;
+    iat: number;
+    exp: number;
+  }
+  
