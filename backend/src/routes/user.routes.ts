d5f43d4489e9456ebb11c3423f540a7abe9b3ef4
Franco du Buisson <franco.dubuisson@bbd.co.za>
2025-05-08 17:56:07 +0200

    more auth stuff

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import { getAllUsers } from '../controllers/user.controller';
+import { authenticateJWT } from '../middlewares/auth.middleware';
 
 const router = Router();
 
+router.get('/me', authenticateJWT, (req, res) => {
+    const user = (req as any).user;
+    res.json({ user });
+  });
+  
 router.get('/', getAllUsers);
 
 export default router;
