8e064e23e6f7ab7800bca23fe29390b41631b5c4
Future-Phahlamohlaka <mamatankane.phahlamohlaka@bbd.co.za>
2025-05-12 19:47:51 +0200

    added login page

--- backend/src/db/pool.ts
+++ backend/src/db/pool.ts
@@ -1,8 +1,12 @@
 import { Pool } from 'pg';
+import { configureEnvironment } from '../utils/env';
+
+const environmentVariables = configureEnvironment();
 
 export const DBPool = new Pool({
-    connectionString: process.env.DB_CONNECTION_STRING,
-    // ssl: {
-    //     rejectUnauthorized: false
-    // }
+    host: environmentVariables.DB_HOST,
+    port: parseInt(environmentVariables.DB_PORT || '5432'),
+    database: environmentVariables.DB_NAME,
+    user: environmentVariables.DB_USER,
+    password: environmentVariables.DB_PASSWORD
 });
