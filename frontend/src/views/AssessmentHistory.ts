0c27085cf84409936f70295411fb16ac99c3926a
Tiya Naidoo <Tiya.Naidoo@bbd.co.za>
2025-05-12 19:21:27 +0200

    Assessment History HTML

--- frontend/src/views/AssessmentHistory.ts
+++ frontend/src/views/AssessmentHistory.ts
@@ -1,22 +1,19 @@
 import '../components/AssessmentFilters.js';
 import '../components/AssessmentList.js';
 import { loadTemplate } from '../utils/load-template.js';
-
 export class AssessmentHistory extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
     this.loadTemplate();
   }
-
   async loadTemplate() {
     const content = await loadTemplate('./templates/assessment-history.view.html');
-    if (content){
+    if (content) {
       this.shadowRoot?.appendChild(content);
     } else {
       // content is null
     }
   }
 }
-
 customElements.define('assessment-history', AssessmentHistory);
