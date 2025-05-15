be0eecd0e2586a218a51bb38308aa15f41a1d1ab
sam <samkele@bbd.co.za>
2025-05-15 19:50:29 +0200

    updated terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,3 +12,13 @@ output "rds_address" {
   description = "PostgreSQL RDS instance endpoint"
   value       = aws_db_instance.doom_db_instance.address
 }
+
+output "website_url" {
+  description = "The full HTTPS URL for the website"
+  value       = "https://${aws_cloudfront_distribution.s3_distribution.domain_name}"
+}
+
+output "backend_url" {
+  description = "The full HTTPS URL for the backend"
+  value       = "https://${aws_cloudfront_distribution.alb_cf_distribution.domain_name}"
+}
